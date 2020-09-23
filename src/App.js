dc1414d0b7f8d83e61726102060af6c46b7db6be
Kevin St.Onge <kevinstonge@gmail.com>
2020-09-23 11:52:04 -0400

    intersectionObserver implementation

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState, useEffect, useRef } from "react";
+import "./App.css";
+/// maybe try this: https://medium.com/@saravananr_93203/infinite-scroll-in-react-made-easy-with-intersection-observer-33bdb5fa9cf6
 function App() {
+  const createListItems = () => {
+    return new Array(5)
+      .fill("")
+      .map((e) => `item #${Math.floor(Math.random() * 10000)}`);
+  };
+  const [ListItems, setListItems] = useState(createListItems());
+  const [isUpdating, setIsUpdating] = useState(false);
+  const [loadingMessage, setLoadingMessage] = useState("... loading more ...");
+  const scrollBoxLastChild = useRef(null);
+  const addListItems = () => {
+    setListItems([...ListItems, ...createListItems()]);
+  };
+  const observerCallback = (entries) => {
+    if (entries[0].isIntersecting && !isUpdating && ListItems.length < 150) {
+      setIsUpdating(true);
+      addListItems();
+      setTimeout(() => {
+        setIsUpdating(false);
+      }, 1000);
+    }
+    if (ListItems.length === 50) {
+      setLoadingMessage("nothing more to load");
+    }
+  };
+  const observer = new IntersectionObserver(observerCallback);
+  useEffect(() => {
+    scrollBoxLastChild && observer.observe(scrollBoxLastChild.current);
+  }, [observer]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <header>number of items in state: {ListItems.length}</header>
+      <div className="scrollBox">
+        {ListItems.map((item, index) => {
+          return (
+            <p key={`${item}-${index}`}>{`paragraph element for ${item}`}</p>
+          );
+        })}
+        <p ref={scrollBoxLastChild}>{loadingMessage}</p>
+      </div>
     </div>
   );
 }
